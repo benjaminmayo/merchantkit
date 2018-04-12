6f0fbbc0404ada90c3d36126bb3949d0821ca8ff
Benjamin Mayo <me@benjaminmayo.co.uk>
2018-04-12 13:13:22 +0100

    Added filter to `AvailablePurchasesTask` to optionally exclude, owned, purchased products. Setting `ignoresPurchasedProducts` to false allows clients to find pricing for currently purchased products, which can be useful for certain user interfaces.

--- Source/Tasks/AvailablePurchasesTask.swift
+++ Source/Tasks/AvailablePurchasesTask.swift
@@ -5,11 +5,14 @@ public final class AvailablePurchasesTask : NSObject, MerchantTask {
     public var onCompletion: TaskCompletion<Purchases>!
     public private(set) var isStarted: Bool = false
     
+    /// If `true`, the task will only return purchases for products that are not already considered purchased by the `Merchant`. Defaults to `true`.
+    public var ignoresPurchasedProducts: Bool = true
+    
     private unowned let merchant: Merchant
     private let products: Set<Product>
     private var skRequest: SKProductsRequest!
     
-    // Create a task using the `Merchant.availablePurchasesTask(forProductIdentifiers(for:)` API. 
+    /// Create a task using the `Merchant.availablePurchasesTask(for:)` API.
     internal init(for products: Set<Product>, with merchant: Merchant) {
         self.products = products
         self.merchant = merchant
@@ -21,11 +24,17 @@ public final class AvailablePurchasesTask : NSObject, MerchantTask {
         self.isStarted = true
         self.merchant.updateActiveTask(self)
         
-        let identifiersForPurchasableProducts = self.products.filter {
-            !self.merchant.state(for: $0).isPurchased
-        }.map { $0.identifier }
+        let productIdentifiers: [String]
+            
+        if self.ignoresPurchasedProducts {
+            productIdentifiers = self.products.filter {
+                !self.merchant.state(for: $0).isPurchased
+            }.map { $0.identifier }
+        } else {
+            productIdentifiers = self.products.map { $0.identifier }
+        }
         
-        self.skRequest = SKProductsRequest(productIdentifiers: Set(identifiersForPurchasableProducts))
+        self.skRequest = SKProductsRequest(productIdentifiers: Set(productIdentifiers))
         self.skRequest.delegate = self
         
         self.skRequest.start()
@@ -41,7 +50,9 @@ public final class AvailablePurchasesTask : NSObject, MerchantTask {
     private func finish(with result: Result<Purchases>) {
         self.onCompletion(result)
         
-        self.merchant.resignActiveTask(self)
+        DispatchQueue.main.async {
+            self.merchant.resignActiveTask(self)
+        }
     }
 }
 
