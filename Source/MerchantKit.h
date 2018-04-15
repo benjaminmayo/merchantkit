bb95b7d0c96243ebc0b23bbd9577c8eb86052f8a
Benjamin Mayo <me@benjaminmayo.co.uk>
2018-04-15 14:23:50 +0100

    Removed errant copyright message.

--- Source/MerchantKit.h
+++ Source/MerchantKit.h
@@ -1,11 +1,3 @@
-//
-//  MerchantKit.h
-//  MerchantKit
-//
-//  Created by Benjamin on 24/04/2017.
-//  Copyright © 2017 Benjamin. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <StoreKit/StoreKit.h>
 
@@ -14,8 +6,6 @@ FOUNDATION_EXPORT double MerchantKitVersionNumber;
 
 //! Project version string for MerchantKit.
 FOUNDATION_EXPORT const unsigned char MerchantKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MerchantKit/PublicHeader.h>
 
 // This trampoline function exists to workaround an incorrect nullability annotation in the `SKProductDiscount` class declaration. Related bug report: rdar://39410422
 static inline NSLocale *_Nullable priceLocaleFromProductDiscount(SKProductDiscount *_Nonnull discount) NS_AVAILABLE_IOS(11_2) {
