9722b2d112614968f680f9e83db0aa8642e68159
Benjamin Mayo <me@benjaminmayo.co.uk>
2018-04-19 12:12:24 +0100

    `SubscriptionPriceFormatter` is now public API. Documentation across both price formatters were standardised and updated.

--- Source/User Interface/PriceFormatter.swift
+++ Source/User Interface/PriceFormatter.swift
@@ -1,12 +1,15 @@
 import Foundation
 
-/// Formats `Price` values into user-facing strings.
+/// Formats `Price` values into user-facing strings. This can be used to display the cost of a `Purchase` to end users. It produces strings like `£3.99` or `$9.99`. 
+///
+/// Typically, you will pass a `Purchase.price` to this formatter for display in the user interface. Use `SubscriptionPriceFormatter` for subscription products where strings like `£3.99 per month` are more appropriate.
+/// - Note: This formatter is currently localized into English, French and Spanish.
 public final class PriceFormatter {
-    /// Text to prepend before the numeric value in the formatted string. Defaults to empty string.
+    /// Text to prepend before the formatted price value. Defaults to empty string.
     public var prefix: String = ""
-    /// Text to append after the numeric value in the formatted string. Defaults to empty string.
+    /// Text to append after the formatted price value. Defaults to empty string.
     public var suffix: String = ""
-    /// Replacement text if the price is free. If used, `prefix` and `suffix` are ignored. Defaults to empty string.
+    /// Replacement text if the price is free. If used by the formatter, `prefix` and `suffix` are ignored. Defaults to empty string.
     public var freeText: String = ""
     
     private let numberFormatter: NumberFormatter = {
--- Source/User Interface/SubscriptionPriceFormatter.swift
+++ Source/User Interface/SubscriptionPriceFormatter.swift
@@ -1,7 +1,7 @@
 /// Formats `Price` values into user-facing strings, incorporating the given `SubscriptionPeriod` into the phrase. This can be used to present the duration of a subscription in the interface. It produces strings like `7 days` or `two weeks`.
-
-internal final class SubscriptionPriceFormatter {
-    public var isSubscriptionFixedDuration: Bool = false
+/// This is typically used to display subscription products, but may have other use cases.
+/// - Note: This formatter is currently localized into English only.
+public final class SubscriptionPriceFormatter {
     public var phrasingStyle: PhrasingStyle = .informal
     
     /// The style of formatting of the unit count. For example, the count could be spelled out in words ('seven days') or represented numerically ('7 days'). Defaults to `numeric`.
@@ -28,34 +28,24 @@ internal final class SubscriptionPriceFormatter {
         return formatter
     }()
     
-    init() {
+    public init() {
         
     }
     
-    func string(from price: Price, period: SubscriptionPeriod) -> String {
+    public func string(from price: Price, duration: SubscriptionDuration) -> String {
         let formattedPrice = self.priceFormatter.string(from: price)
-        let formattedUnitCount = self.formattedUnitCount(from: period.unitCount)
+        let formattedUnitCount = self.formattedUnitCount(from: duration.period.unitCount)
         
         let localizedStringSource = LocalizedStringSource(for: self.locale)
         
         let configuration = LocalizedStringSource.SubscriptionPricePhraseConfiguration(
-            period: period,
-            isFormal: self.phrasingStyle == .formal,
-            isFixedDuration: self.isSubscriptionFixedDuration
+            duration: duration,
+            isFormal: self.phrasingStyle == .formal
         )
         
         let phrase = localizedStringSource.subscriptionPricePhrase(with: configuration, formattedPrice: formattedPrice, formattedUnitCount: formattedUnitCount)
+        
         return phrase
-        
-        //                  |Informal                 |Formal                |
-        
-        // * Price: £3.99, Period: 1 month *
-        // |isFixedPeriod   |£3.99 for 1 month        |£3.99 for 1 month     |
-        // |isNotFixedPeriod|£3.99 a month            |£3.99 per month       |
-        
-        // * Price: £4.99, Period: 3 months *
-        // |isFixedPeriod   |£3.99 for 3 months       |£3.99 for 3 months    |
-        // |isNotFixedPeriod|£3.99 every 3 months     |£3.99 every 3 months  |
     }
     
     public enum PhrasingStyle {
@@ -82,7 +72,6 @@ extension SubscriptionPriceFormatter {
         
         self.unitCountFormatter.numberStyle = numberStyle
     }
-    
     
     private func formattedUnitCount(from unitCount: Int) -> String {
         let formattedString = self.unitCountFormatter.string(from: unitCount as NSNumber)!
