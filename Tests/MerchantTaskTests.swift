0cd873c05f372d2e32d3ac778088ecf578384a6f
Benjamin Mayo <me@benjaminmayo.co.uk>
2018-04-27 22:35:45 +0100

    Added test for `MerchantTaskUtilities` assertion.

---
+++ Tests/MerchantTaskTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+import Foundation
+@testable import MerchantKit
+
+class MerchantTaskTests : XCTestCase {
+    func testMerchantTaskAssertIfStartedBefore() {
+        let expectation = self.expectation(description: "fatalError thrown")
+        
+        MerchantKitFatalError.customHandler = {
+            expectation.fulfill()
+        }
+        
+        let testingQueue = DispatchQueue(label: "testing queue") // testing MerchantKitFatalError requires dispatch to a non-main thread
+
+        testingQueue.async {
+            let task = MockMerchantTask()
+            task.start()
+            task.start() // should trap here
+        }
+        
+        self.wait(for: [expectation], timeout: 2)
+    }
+}
+
+private class MockMerchantTask : MerchantTask {
+    var isStarted: Bool = false
+    
+    init() {
+        
+    }
+    
+    func start() {
+        self.assertIfStartedBefore()
+        
+        self.isStarted = true
+    }
+}
