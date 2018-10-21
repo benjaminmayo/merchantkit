55f499eed6207c5c7619f1adba08edce582b94cf
Benjamin Mayo <me@benjaminmayo.co.uk>
2018-10-21 21:06:48 +0100

    Added a public initializer for `SubscriptionPeriod`.

--- Source/Model/SubscriptionPeriod.swift
+++ Source/Model/SubscriptionPeriod.swift
@@ -3,11 +3,23 @@ public struct SubscriptionPeriod : Hashable {
     public let unit: Unit
     public let unitCount: Int
     
-    internal init(unit: Unit, unitCount: Int) {
+    /// Create a `SubscriptionPeriod` with the given `unit` and `unitCount`. A `Unit.day` with a count of 5 represents five days, for example.
+    // Typically, you do not need to construct `SubscriptionPeriod` instances manually.
+    public init(unit: Unit, unitCount: Int) {
         self.unit = unit
         self.unitCount = unitCount
     }
     
+    public enum Unit : Equatable {
+        case day
+        case week
+        case month
+        case year
+    }
+}
+
+// internal convenience constructors that may warrant becoming public in future
+extension SubscriptionPeriod {
     internal static func days(_ unitCount: Int) -> SubscriptionPeriod {
         return SubscriptionPeriod(unit: .day, unitCount: unitCount)
     }
@@ -24,11 +36,4 @@ public struct SubscriptionPeriod : Hashable {
         return SubscriptionPeriod(unit: .year, unitCount: unitCount)
     }
     
-    public enum Unit : Equatable {
-        case day
-        case week
-        case month
-        case year
-    }
 }
-
