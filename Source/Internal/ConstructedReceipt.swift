885a48bc1b251288bdf674175c4e57b59a279cf6
Benjamin Mayo <me@benjaminmayo.co.uk>
2019-02-10 00:28:08 +0000

    Removed `Buckets` internal data type, which predated newer Dictionary default subscript API being added to the Swift standard library.

--- Source/Internal/ConstructedReceipt.swift
+++ Source/Internal/ConstructedReceipt.swift
@@ -4,24 +4,24 @@ internal struct ConstructedReceipt : Receipt, CustomStringConvertible, CustomDeb
     public let metadata: ReceiptMetadata
     
     var productIdentifiers: Set<String> {
-        return self.entries.keys
+        return Set(self.entries.keys)
     }
     
-    private let entries: Buckets<String, ReceiptEntry>
+    private let entries: [String : [ReceiptEntry]]
     
     init(from allEntries: [ReceiptEntry], metadata: ReceiptMetadata) {
         self.metadata = metadata
         
-        var entriesForProductIdentifier = Buckets<String, ReceiptEntry>()
+        var entriesForProductIdentifier = [String : [ReceiptEntry]]()
         
         for entry in allEntries {
-            entriesForProductIdentifier[entry.productIdentifier].append(entry)
+            entriesForProductIdentifier[entry.productIdentifier, default: []].append(entry)
         }
         
         self.entries = entriesForProductIdentifier
     }
     
     func entries(forProductIdentifier productIdentifier: String) -> [ReceiptEntry] {
-        return self.entries[productIdentifier]
+        return self.entries[productIdentifier, default: []]
     }
 }
