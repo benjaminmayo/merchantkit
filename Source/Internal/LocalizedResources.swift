a9dd5ec7abd539465d117f24d8dd472e17b78b29
Benjamin Mayo <me@benjaminmayo.co.uk>
2019-02-17 00:34:53 +0000

    Commented out unused `LocalizedStringProvider` implementation.

--- Source/Internal/LocalizedResources.swift
+++ Source/Internal/LocalizedResources.swift
@@ -115,123 +115,123 @@ fileprivate protocol LocalizedStringProvider {
     func localizedString(for key: LocalizedStringSource.Key, formatting arguments: [CVarArg]) -> String
 }
 
-/// This handles the replacement logic that Foundation does 'magically'. The downside to this approach is that MerchantKit becomes responsible for handling pluralization rules. This kinda sucks but the Foundation approach is flimsy.
-fileprivate final class InternalLocalizedStringProvider : LocalizedStringProvider {
-    private let locale: Locale
-    private let bundle: Bundle
-    
-    private var _cachedResourceDicts = [String : [String : Any]]()
-
-    init(locale: Locale, bundle: Bundle) {
-        self.locale = locale
-        self.bundle = bundle
-    }
-    
-    func localizedString(for key: LocalizedStringSource.Key, formatting arguments: [CVarArg]) -> String {
-        enum FormattedLocalizedStringError : Swift.Error {
-            case urlNotFound(resourceName: String)
-            case plistWrongFormat
-            case missingValue(key: String)
-            case unexpectedValue(key: String)
-        }
-        
-        do {
-            let resourceName = "MerchantKitResources\(key.tableName)"
-            
-            let dict: [String : Any]
-            
-            if let cached = self._cachedResourceDicts[resourceName] {
-                dict = cached
-            } else {
-                guard let url = self.bundle.url(forResource: resourceName, withExtension: "stringsdict", subdirectory: "") else { throw FormattedLocalizedStringError.urlNotFound(resourceName: resourceName) }
-                
-                let data = try Data(contentsOf: url)
-                guard let decoded = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String : Any] else {
-                    throw FormattedLocalizedStringError.plistWrongFormat
-                }
-                
-                dict = decoded
-            }
-            
-            guard let formatContainer = dict[key.stringValue] as? [String : Any] else {
-                throw FormattedLocalizedStringError.unexpectedValue(key: key.stringValue)
-            }
-            
-            let localizedFormatKey = "NSStringLocalizedFormatKey"
-            
-            guard let rawFormat = formatContainer[localizedFormatKey] as? String else {
-                throw FormattedLocalizedStringError.unexpectedValue(key: localizedFormatKey)
-            }
-            
-            var replacementData = [(key: String, replacement: String)]()
-            
-            if formatContainer.count > 1 {
-                let pluralizations = self.acceptablePluralizations(for: arguments.first as! Int)
-                
-                for (replacementKey, object) in formatContainer {
-                    guard replacementKey != localizedFormatKey else { continue }
-                    
-                    if let object = object as? [String : String] {
-                        guard let stringValue = self.stringValue(forPluralizationsList: object, selectingFrom: pluralizations) else {
-                            throw FormattedLocalizedStringError.missingValue(key: replacementKey)
-                        }
-                        
-                        replacementData.append((replacementKey, stringValue))
-                    }
-                }
-            }
-            
-            var replacedFormat = rawFormat
-            
-            for (key, replacement) in replacementData {
-                replacedFormat = replacedFormat.replacingOccurrences(of: "%#@\(key)@", with: replacement)
-            }
-            
-            for (index, argument) in arguments.enumerated() {
-                replacedFormat = replacedFormat.replacingOccurrences(of: "%\(index + 1)$@", with: String(describing: argument as Any))
-            }
-            
-            let format = replacedFormat
-            let result = String(format: format, arguments: arguments)
-            
-            return result
-        } catch let error {
-            print(error)
-            
-            return "Unhandled+\(key.stringValue)"
-        }
-    }
-    
-    private func acceptablePluralizations(for count: Int) -> [Pluralization] {
-        // this will need to be expanded to support more locales eventually
-        
-        switch count {
-            case 1:
-                return [.one, .other]
-            default:
-                return [.other]
-        }
-    }
-    
-    private func stringValue(forPluralizationsList object: [String : String], selectingFrom acceptablePluralizations: [Pluralization]) -> String? {
-        for pluralization in acceptablePluralizations {
-            if let value = object[pluralization.rawValue] {
-                return value
-            }
-        }
-        
-        return nil
-    }
-    
-    private enum Pluralization : String {
-        case zero
-        case one
-        case two
-        case few
-        case many
-        case other
-    }
-}
+/// This handles the replacement logic that Foundation does 'magically'. The downside to this approach is that MerchantKit becomes responsible for handling pluralization rules. This kinda sucks but maybe needed in the future if the Foundation approach becomes untenable.
+//fileprivate final class InternalLocalizedStringProvider : LocalizedStringProvider {
+//    private let locale: Locale
+//    private let bundle: Bundle
+//
+//    private var _cachedResourceDicts = [String : [String : Any]]()
+//
+//    init(locale: Locale, bundle: Bundle) {
+//        self.locale = locale
+//        self.bundle = bundle
+//    }
+//
+//    func localizedString(for key: LocalizedStringSource.Key, formatting arguments: [CVarArg]) -> String {
+//        enum FormattedLocalizedStringError : Swift.Error {
+//            case urlNotFound(resourceName: String)
+//            case plistWrongFormat
+//            case missingValue(key: String)
+//            case unexpectedValue(key: String)
+//        }
+//
+//        do {
+//            let resourceName = "MerchantKitResources\(key.tableName)"
+//
+//            let dict: [String : Any]
+//
+//            if let cached = self._cachedResourceDicts[resourceName] {
+//                dict = cached
+//            } else {
+//                guard let url = self.bundle.url(forResource: resourceName, withExtension: "stringsdict", subdirectory: "") else { throw FormattedLocalizedStringError.urlNotFound(resourceName: resourceName) }
+//
+//                let data = try Data(contentsOf: url)
+//                guard let decoded = try PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String : Any] else {
+//                    throw FormattedLocalizedStringError.plistWrongFormat
+//                }
+//
+//                dict = decoded
+//            }
+//
+//            guard let formatContainer = dict[key.stringValue] as? [String : Any] else {
+//                throw FormattedLocalizedStringError.unexpectedValue(key: key.stringValue)
+//            }
+//
+//            let localizedFormatKey = "NSStringLocalizedFormatKey"
+//
+//            guard let rawFormat = formatContainer[localizedFormatKey] as? String else {
+//                throw FormattedLocalizedStringError.unexpectedValue(key: localizedFormatKey)
+//            }
+//
+//            var replacementData = [(key: String, replacement: String)]()
+//
+//            if formatContainer.count > 1 {
+//                let pluralizations = self.acceptablePluralizations(for: arguments.first as! Int)
+//
+//                for (replacementKey, object) in formatContainer {
+//                    guard replacementKey != localizedFormatKey else { continue }
+//
+//                    if let object = object as? [String : String] {
+//                        guard let stringValue = self.stringValue(forPluralizationsList: object, selectingFrom: pluralizations) else {
+//                            throw FormattedLocalizedStringError.missingValue(key: replacementKey)
+//                        }
+//
+//                        replacementData.append((replacementKey, stringValue))
+//                    }
+//                }
+//            }
+//
+//            var replacedFormat = rawFormat
+//
+//            for (key, replacement) in replacementData {
+//                replacedFormat = replacedFormat.replacingOccurrences(of: "%#@\(key)@", with: replacement)
+//            }
+//
+//            for (index, argument) in arguments.enumerated() {
+//                replacedFormat = replacedFormat.replacingOccurrences(of: "%\(index + 1)$@", with: String(describing: argument as Any))
+//            }
+//
+//            let format = replacedFormat
+//            let result = String(format: format, arguments: arguments)
+//
+//            return result
+//        } catch let error {
+//            print(error)
+//
+//            return "Unhandled+\(key.stringValue)"
+//        }
+//    }
+//
+//    private func acceptablePluralizations(for count: Int) -> [Pluralization] {
+//        // this will need to be expanded to support more locales eventually
+//
+//        switch count {
+//            case 1:
+//                return [.one, .other]
+//            default:
+//                return [.other]
+//        }
+//    }
+//
+//    private func stringValue(forPluralizationsList object: [String : String], selectingFrom acceptablePluralizations: [Pluralization]) -> String? {
+//        for pluralization in acceptablePluralizations {
+//            if let value = object[pluralization.rawValue] {
+//                return value
+//            }
+//        }
+//
+//        return nil
+//    }
+//
+//    private enum Pluralization : String {
+//        case zero
+//        case one
+//        case two
+//        case few
+//        case many
+//        case other
+//    }
+//}
 
 /// This handles localized string replacement. Unfortunately, it is kind of a black box and sometimes unreliable. For now, we include this implementation as a backup — with a view to select a single concrete provider down the road.
 fileprivate final class FoundationLocalizedStringProvider : LocalizedStringProvider {
