5cddfa3d899e3aaf005c6aca7e90793dff546ee2
Benjamin Mayo <me@benjaminmayo.co.uk>
2019-05-29 12:25:21 +0100

    Added support for describing subscription discounts as `SubscriptionTerms.availableRetentionOffers`.

--- Source/Model/SubscriptionTerms.swift
+++ Source/Model/SubscriptionTerms.swift
@@ -1,12 +1,28 @@
-public struct SubscriptionTerms: Equatable {
+public struct SubscriptionTerms : Equatable {
     public let duration: SubscriptionDuration
     
-    public let introductoryOffer: Discount?
-    public let discounts: [Discount]?
+    public let introductoryOffer: IntroductoryOffer?
+    public let availableRetentionOffers: [RetentionOffer]
     
-    public enum Discount: Equatable {
+    public enum IntroductoryOffer : Equatable {
         case freeTrial(period: SubscriptionPeriod)
         case upfrontDiscount(discountedPrice: Price, period: SubscriptionPeriod)
         case recurringDiscount(discountedPrice: Price, recurringPeriod: SubscriptionPeriod, discountedPeriodCount: Int)
+    }
+    
+    public struct RetentionOffer : Equatable {
+        public let identifier: String
+        public let discount: Discount
+        
+        public init(identifier: String, discount: Discount) {
+            self.identifier = identifier
+            self.discount = discount
+        }
+        
+        public enum Discount : Equatable {
+            case freeTerm(period: SubscriptionPeriod)
+            case upfront(discountedPrice: Price, period: SubscriptionPeriod)
+            case recurring(discountedPrice: Price, period: SubscriptionPeriod, discountedPeriodCount: Int)
+        }
     }
 }
