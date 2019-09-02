816498ba60e45693141bc017aa66f2672a5d3f7d
Hal Lee <hal@lee.me>
2019-09-02 16:38:20 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "MerchantKit",
+    platforms: [.iOS(.v11), .macOS(.v10_14)],
+    products: [
+        .library(name: "MerchantKit", targets: ["MerchantKit"])
+    ],
+    targets: [
+        .target(name: "MerchantKit", path: "Source")
+    ]
+)
