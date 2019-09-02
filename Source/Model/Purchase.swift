bf02eaa3de0c07d11ee1ea85f91cb28383d61024
Hal Lee <hal@lee.me>
2019-09-02 17:09:05 -0400

    Add @available

--- Source/Model/Purchase.swift
+++ Source/Model/Purchase.swift
@@ -139,6 +139,7 @@ public struct Purchase : Hashable, CustomStringConvertible {
     // This trampoline function exists to workaround an incorrect nullability annotation in the `SKProductDiscount` class declaration.
     // Related bug report: rdar://39410422 (update: now closed)
     // According to Apple engineering, this bug was fixed with iOS 12.0. As the project supports older OS versions, we can't remove this indirection quite yet.
+    @available(iOS 11.2, *)
     func priceLocale(from productDiscount: SKProductDiscount) -> Locale {
         if #available(iOS 12, *) {
             return productDiscount.priceLocale
