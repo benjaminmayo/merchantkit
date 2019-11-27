4639d8c2d9567058fcdc74814a27dfe15eeae0fe
Benjamin Mayo <me@benjaminmayo.co.uk>
2019-11-27 20:58:06 +0000

    added another leeway duration test case

--- Tests/Test Cases/ReceiptValidatorSubscriptionRenewalLeewayTests.swift
+++ Tests/Test Cases/ReceiptValidatorSubscriptionRenewalLeewayTests.swift
@@ -21,4 +21,54 @@ class ReceiptValidatorSubscriptionRenewalLeewayTests : XCTestCase {
         
         MerchantKitFatalError.customHandler = nil
     }
+    
+    func testMerchantRespectsLeewayDuration() {
+        let interval: TimeInterval = (60 * 60 * 24)
+        let maximum = 100
+        
+        for index in 0..<(maximum/2) {
+            let entries = (0..<maximum).map { index in
+                ReceiptEntry(productIdentifier: "\(index)", expiryDate: Date().addingTimeInterval(-TimeInterval(index) * interval))
+            }
+            
+            let mockValidator = MockReceiptValidator()
+            mockValidator.validateRequest = { (_, completion) in
+                let receipt = ConstructedReceipt(from: entries, metadata: .init(originalApplicationVersion: "1", bundleIdentifier: "", creationDate: Date()))
+
+                completion(.success(receipt))
+            }
+
+            let products = entries.map({ $0.productIdentifier }).map { productIdentifier in
+                Product(identifier: productIdentifier, kind: .subscription(automaticallyRenews: true))
+            }
+            
+            let expectation = self.expectation(description: "expected unlocked subscription for products up to index \(index)")
+            expectation.expectedFulfillmentCount = index + 1
+            expectation.assertForOverFulfill = true
+            mockValidator.subscriptionRenewalLeeway = .init(allowedElapsedDuration: 1.0 + (TimeInterval(index) * interval))
+            
+            let mockStoreInterface = MockStoreInterface()
+            mockStoreInterface.receiptFetchResult = .success(Data())
+            
+            var subscribedProducts = Set(products[0...index])
+
+            var merchant: Merchant!
+            let mockDelegate = MockMerchantDelegate()
+            mockDelegate.didChangeStates = { products in
+                for product in products where merchant.state(for: product).isPurchased {
+                    if subscribedProducts.remove(product) != nil {
+                        expectation.fulfill()
+                    }
+                }
+            }
+            
+            let configuration = Merchant.Configuration(receiptValidator: mockValidator, storage: EphemeralPurchaseStorage())
+            merchant = Merchant(configuration: configuration, delegate: mockDelegate, consumableHandler: nil, storeInterface: mockStoreInterface)
+            
+            merchant.register(products)
+            merchant.setup()
+            
+            self.wait(for: [expectation], timeout: 5)
+        }
+    }
 }
